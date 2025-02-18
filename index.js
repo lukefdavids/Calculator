e0b4513ef5803e5817d02df91917fddc668973a8
Luke Davids <lukedavids@gmail.com>
2025-02-17 21:37:08 -0600

    "Sum" update
    Updated Sum to read "Sum = "

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ let sumEl = document.getElementById("sum-el")
 
 function add() {
     let result = num1 + num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum = " + result
 }
 
 function subtract() {
     let result = num1 - num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum = " + result
 }
 
 function divide() {
     let result = num1 / num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum = " + result
 }
 
 function multiply() {
     let result = num1 * num2
-    sumEl.textContent = "Sum: " + result
+    sumEl.textContent = "Sum = " + result
 }
 
